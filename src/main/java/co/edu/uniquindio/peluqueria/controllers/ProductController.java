4c44b829019d73aaf678fe2e265503c597c1bec6
Tourment0412 <94409923+Tourment0412@users.noreply.github.com>
2024-09-29 19:03:58 -0500

    Add Rest Controller for Product

--- src/main/java/co/edu/uniquindio/peluqueria/controllers/ProductController.java
+++ src/main/java/co/edu/uniquindio/peluqueria/controllers/ProductController.java
@@ -1,4 +1,44 @@
 package co.edu.uniquindio.peluqueria.controllers;
 
+
+import co.edu.uniquindio.peluqueria.dto.productdto.ProductItemDTO;
+import co.edu.uniquindio.peluqueria.dto.productdto.RegisterProductDTO;
+import co.edu.uniquindio.peluqueria.model.documents.Product;
+import co.edu.uniquindio.peluqueria.services.interfaces.ProductService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
 public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerProduct(@RequestBody RegisterProductDTO registerProductDTO) {
+        try {
+            String productId= productService.registerProduct(registerProductDTO);
+            return ResponseEntity.status(HttpStatus.CREATED).body(productId);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getAllProducts() {
+        try {
+            List<ProductItemDTO> products = productService.getAllProducts();
+            return ResponseEntity.ok(products);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
+    }
+
 }
