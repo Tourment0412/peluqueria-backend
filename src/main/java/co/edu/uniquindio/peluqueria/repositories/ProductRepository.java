f10049ee8da8203b01a90cc6e26c16392204da83
Tourment0412 <94409923+Tourment0412@users.noreply.github.com>
2024-09-29 19:03:02 -0500

    Add product repository

--- src/main/java/co/edu/uniquindio/peluqueria/repositories/ProductRepository.java
+++ src/main/java/co/edu/uniquindio/peluqueria/repositories/ProductRepository.java
@@ -5,9 +5,13 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ProductRepository extends MongoRepository<Product, String> {
 
+
+    Optional<Product> findByName(String name);
 
 
 }
