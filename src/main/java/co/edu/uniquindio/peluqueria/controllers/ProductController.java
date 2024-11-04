316273548ac42736247da740d3b580684e610375
Tourment0412 <94409923+Tourment0412@users.noreply.github.com>
2024-11-04 12:43:43 -0500

    Update ProductController.java

--- src/main/java/co/edu/uniquindio/peluqueria/controllers/ProductController.java
+++ src/main/java/co/edu/uniquindio/peluqueria/controllers/ProductController.java
@@ -2,6 +2,7 @@ package co.edu.uniquindio.peluqueria.controllers;
 
 
 import co.edu.uniquindio.peluqueria.dto.messages.MessageDTO;
+import co.edu.uniquindio.peluqueria.dtos.productdto.ProductFilterDTO;
 import co.edu.uniquindio.peluqueria.dtos.productdto.ProductItemDTO;
 import co.edu.uniquindio.peluqueria.dtos.productdto.RegisterProductDTO;
 import co.edu.uniquindio.peluqueria.services.interfaces.ProductService;
@@ -30,6 +31,17 @@ public class ProductController {
             return ResponseEntity.badRequest().body(new MessageDTO<>(true, e.getMessage()));
         }
     }
+
+    @PostMapping("/filter-products")
+    public ResponseEntity<MessageDTO<List<ProductItemDTO>>> filterProducts(@RequestBody ProductFilterDTO productFilterDTO){
+        try {
+            List<ProductItemDTO> listFiltered= productService.filterProducts(productFilterDTO);
+            return ResponseEntity.ok(new MessageDTO<>(false, listFiltered));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(new MessageDTO<>(true, null));
+        }
+    }
+
 
     @GetMapping("/get-all")
     public ResponseEntity<MessageDTO<List<ProductItemDTO>>> getAllProducts() {
